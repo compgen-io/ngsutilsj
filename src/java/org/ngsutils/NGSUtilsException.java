c8b1478bb6117064867371fde0108f126ff85735
Marcus Breese <mbreese@stanford.edu>
2014-03-31 21:21:09 -0700

    Added exception ctor

--- src/java/org/ngsutils/NGSUtilsException.java
+++ src/java/org/ngsutils/NGSUtilsException.java
@@ -4,6 +4,9 @@ public class NGSUtilsException extends Exception {
 	public NGSUtilsException(String msg) {
 		super(msg);
 	}
+	public NGSUtilsException(Exception e) {
+		super(e);
+	}
 
 	private static final long serialVersionUID = 6388968582218017316L;
 
