99e99aae0afdc48d905fadba427494316791f2e3
Marcus Breese <mbreese@stanford.edu>
2014-04-16 00:09:02 -0700

    Added option for applying filter to read1 or read2. This way we could use the same filter class, but apply two different filters to interlaced files.

--- src/java/org/ngsutils/cli/fastq/filter/SeqTrimFilter.java
+++ src/java/org/ngsutils/cli/fastq/filter/SeqTrimFilter.java
@@ -4,22 +4,46 @@ import org.ngsutils.fastq.FastqRead;
 import org.ngsutils.support.StringUtils;
 
 public class SeqTrimFilter extends AbstractSingleReadFilter {
-    private int minOverlap;
-    private double minPct;
-	private String trimSeq;
+    final private int minOverlap;
+    final private double minPct;
+    final private int readNum;
+	final private String trimSeq;
+	private String lastName=null;
 
-	public SeqTrimFilter(Iterable<FastqRead> parent, boolean verbose, String trimSeq, int minOverlap, double minPct) {
-		super(parent, verbose);
-		this.trimSeq = trimSeq.toUpperCase();
-		this.minOverlap = minOverlap;
-		this.minPct = minPct;
+    public SeqTrimFilter(Iterable<FastqRead> parent, boolean verbose, String trimSeq, int minOverlap, double minPct, int readNum) {
+        super(parent, verbose);
+        this.trimSeq = trimSeq.toUpperCase();
+        this.minOverlap = minOverlap;
+        this.minPct = minPct;
+        this.readNum = readNum;
         if (verbose) {
             System.err.println("["+this.getClass().getSimpleName()+"] Removing: " + this.trimSeq + " from 3' end of reads with an overlap of at least:" + minOverlap+ " with a minimum match pct: "+ minPct);
         }
-	}
+    }
+
+    public SeqTrimFilter(Iterable<FastqRead> parent, boolean verbose, String trimSeq, int minOverlap, double minPct) {
+        super(parent, verbose);
+        this.trimSeq = trimSeq.toUpperCase();
+        this.minOverlap = minOverlap;
+        this.minPct = minPct;
+        this.readNum = 0;
+        if (verbose) {
+            System.err.println("["+this.getClass().getSimpleName()+"] Removing: " + this.trimSeq + " from 3' end of reads with an overlap of at least:" + minOverlap+ " with a minimum match pct: "+ minPct);
+        }
+    }
 
 	@Override
 	protected FastqRead filterRead(FastqRead read) {
+	    int readNum = 1;
+	    if (lastName != null && read.getName().equals(lastName)) {
+	        readNum = 2;
+	    }
+	    lastName = read.getName();
+	    
+	    if (this.readNum != 0 && this.readNum != readNum) {
+	        return read;
+	    }
+	    
 	    int i = minOverlap;
 	    while (i < read.getSeq().length()) {
 	        String subseq = StringUtils.sliceRight(read.getSeq(), -i);
