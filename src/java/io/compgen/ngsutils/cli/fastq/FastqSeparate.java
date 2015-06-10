2cb9f5c45a345d4cba760d3e00ef3188ab4b0170
Marcus Breese <mbreese@stanford.edu>
2015-06-10 13:08:44 -0700

    updated fastq separate to output both reads to files at the same time, if needed

--- src/java/io/compgen/ngsutils/cli/fastq/FastqSeparate.java
+++ src/java/io/compgen/ngsutils/cli/fastq/FastqSeparate.java
@@ -5,52 +5,68 @@ import io.compgen.cmdline.annotation.Exec;
 import io.compgen.cmdline.annotation.Option;
 import io.compgen.cmdline.annotation.UnnamedArg;
 import io.compgen.cmdline.exceptions.CommandArgumentException;
-import io.compgen.cmdline.impl.AbstractOutputCommand;
+import io.compgen.cmdline.impl.AbstractCommand;
 import io.compgen.common.Counter;
+import io.compgen.common.io.FileUtils;
 import io.compgen.ngsutils.fastq.Fastq;
 import io.compgen.ngsutils.fastq.FastqRead;
 import io.compgen.ngsutils.fastq.FastqReader;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 @Command(name = "fastq-separate", desc = "Splits an interleaved FASTQ file by read number.", category="fastq")
-public class FastqSeparate extends AbstractOutputCommand {
+public class FastqSeparate extends AbstractCommand {
 	private String filename = null;
 	
-	private boolean readOne = false;
-	private boolean readTwo = false;
-
+    private String outfile1 = null;
+    private String outfile2 = null;
+    
 	public FastqSeparate() {
 	}
 
-	@UnnamedArg(name = "FILE")
+	@UnnamedArg(name = "fastqfile")
 	public void setFilename(String filename) throws IOException {
 	    this.filename = filename;
 	}
 
-	@Option(desc="Export read 1", charName = "1", name="read1")
-	public void setReadOne(boolean value) {
-		this.readOne = value;
-	}
+    @Option(desc="Export read 1 to this file (use - for stdout)", name="read1", required=false)
+    public void setReadOne(String outfile1) {
+        this.outfile1 = outfile1;
+    }
 
-	@Option(desc="Export read 2", charName = "2", name="read2")
-	public void setReadTwo(boolean value) {
-		this.readTwo = value;
-	}
+    @Option(desc="Export read 2 to this file (use - for stdout)", name="read2", required=false)
+    public void setReadTwo(String outfile2) {
+        this.outfile2 = outfile2;
+    }
 
     @Exec
 	public void exec() throws IOException, CommandArgumentException {
-        if ((!readOne && !readTwo) || (readOne && readTwo)) {
-            throw new CommandArgumentException("You must specify at one (and only one) read to export (-1 or -2)");
+        if (outfile1 == null && outfile2 == null) {
+            throw new CommandArgumentException("You must specify at least one read to export");
+        }
+
+        if ((outfile1 != null && outfile1.equals("-")) && (outfile2 != null && outfile2.equals("-"))) {
+            throw new CommandArgumentException("Both reads can not be exported to stdout!");
         }
         
+        OutputStream out1 = null;
+        OutputStream out2 = null;
+                
 		if (verbose) {
 			System.err.println("Spliting file:" + filename);
-            if (readOne) {
-				System.err.println("Exporting read 1");
-			} else {
-				System.err.println("Exporting read 2");
-			}
+		}
+        if (outfile1 != null) {
+            if (verbose) {
+                System.err.println("Exporting read 1 to " + outfile1);
+            }
+            out1 = FileUtils.openOutputStream(outfile1);
+        }
+        if (outfile2 != null) {
+            if (verbose) {
+                System.err.println("Exporting read 2 to " + outfile2);
+            }
+            out2 = FileUtils.openOutputStream(outfile2);
 		}
 
 	    FastqReader reader = Fastq.open(filename);
@@ -58,12 +74,13 @@ public class FastqSeparate extends AbstractOutputCommand {
 		Counter counter = new Counter();
 		boolean isRead1 = true;
 		for (FastqRead read : reader) {
-			if (readOne && isRead1) {
-				counter.incr();
-				read.write(out);
-			} else if (readTwo && !isRead1) {
-				counter.incr();
-				read.write(out);
+		    if (isRead1) {
+	            counter.incr();
+		    }
+			if (out1 != null && isRead1) {
+				read.write(out1);
+			} else if (out2 != null && !isRead1) {
+				read.write(out2);
 			}
 			isRead1 = !isRead1;
 		}
@@ -72,7 +89,12 @@ public class FastqSeparate extends AbstractOutputCommand {
 			System.err.println("Total reads: " + counter.getValue());
 		}
 		reader.close();
-		close();
+        if (out1 != null && out1 != System.out) {
+            out1.close();
+        }
+        if (out2 != null && out2 != System.out) {
+            out2.close();
+        }
 	}
 
 }
