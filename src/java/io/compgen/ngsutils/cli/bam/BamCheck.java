f0cb4518769acbc1dc05557955a0827a4efdc548
Marcus Breese <mbreese@stanford.edu>
2016-02-18 01:43:01 -0500

    added check for missing mates

--- src/java/io/compgen/ngsutils/cli/bam/BamCheck.java
+++ src/java/io/compgen/ngsutils/cli/bam/BamCheck.java
@@ -11,9 +11,11 @@ import io.compgen.cmdline.annotation.Option;
 import io.compgen.cmdline.annotation.UnnamedArg;
 import io.compgen.cmdline.exceptions.CommandArgumentException;
 import io.compgen.cmdline.impl.AbstractCommand;
+import io.compgen.common.RadixSet;
 import io.compgen.common.progress.FileChannelStats;
 import io.compgen.common.progress.ProgressMessage;
 import io.compgen.common.progress.ProgressUtils;
+import io.compgen.ngsutils.bam.support.ReadUtils;
 import io.compgen.ngsutils.support.CloseableFinalizer;
 
 import java.io.File;
@@ -27,10 +29,17 @@ public class BamCheck extends AbstractCommand {
     private String filename = null;
     private boolean lenient = false;
     private boolean silent = false;
+    private boolean mates = false;
 
+    
     @UnnamedArg(name = "FILE")
     public void setFilename(String filename) {
         this.filename = filename;
+    }
+
+    @Option(desc = "Check for missing read-mates (paired-end)", name="mates")
+    public void setMates(boolean mates) {
+        this.mates = mates;
     }
 
     @Option(desc = "Use lenient validation strategy", name="lenient")
@@ -69,20 +78,57 @@ public class BamCheck extends AbstractCommand {
             name = f.getName();
         }
 
+        final RadixSet setOne = new RadixSet();
+        final RadixSet setTwo = new RadixSet();
+        final RadixSet goodReads = new RadixSet();
 
         Iterator<SAMRecord> it = ProgressUtils.getIterator(name, reader.iterator(), (channel == null)? null : new FileChannelStats(channel), new ProgressMessage<SAMRecord>() {
             long i = 0;
             @Override
             public String msg(SAMRecord current) {
                 i++;
-                return i+" "+current.getReadName();
+                return i+" "+current.getReadName() + " "+setOne.size()+"/"+setTwo.size()+"/"+goodReads.size();
             }}, new CloseableFinalizer<SAMRecord>());
         long i = 0;
         while (it.hasNext()) {
             i++;
-            it.next();
+            SAMRecord read = it.next();
+            
+            if (read.getReadPairedFlag() && mates) {
+                String readname = read.getReadName();
+                if (!goodReads.contains(readname)) {
+                    if (read.getFirstOfPairFlag()) {
+                        if (setTwo.contains(readname)) {
+                            setTwo.remove(readname);
+                            if (!ReadUtils.isReadUniquelyMapped(read)) {
+                                goodReads.add(readname);
+                            }
+                        } else {
+                            setOne.add(readname);
+                        }
+                    } else {
+                        if (setOne.contains(readname)) {
+                            setOne.remove(readname);
+                            if (!ReadUtils.isReadUniquelyMapped(read)) {
+                                goodReads.add(readname);
+                            }
+                        } else {
+                            setTwo.add(readname);
+                        }
+                    }
+                }
+            }
         }
         reader.close();
         System.err.println("Successfully read: "+i+" records.");
+        if (mates) {
+            System.err.println("Reads with missing mates: "+ (setOne.size()+setTwo.size()));
+            if (setOne.size() > 0) {
+                Iterator<String> it2 = setOne.iterator();
+                for (int j=0; j<10 && setOne.size() > j; j++) {
+                    System.err.println("Example read: "+it2.next());
+                }
+            }
+        }
     }
 }
