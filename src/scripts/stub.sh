e716d1549297c1c9fd8356d9569e8b7bf5fbc988
Marcus R. Breese <marcus.breese@ucsf.edu>
2017-06-21 13:16:02 -0400

    updated shell stub to set MALLOC_ARENA_MAX and ParallelGCThreads

--- src/scripts/stub.sh
+++ src/scripts/stub.sh
@@ -23,6 +23,24 @@ if [ ! -t 2 ]; then
     JAVA_OPTS="${JAVA_OPTS} -Dio.compgen.common.tty.fd2=F"
 fi
 
+# limit MALLOC_ARENA to keep vmem under control on bigmem systems
+if [ "$MALLOC_ARENA_MAX" = "" ]; then
+    export MALLOC_ARENA_MAX=4
+fi
+
+# limit parallel gc threads (systems with lots of processors)
+FOUNDGC=0
+case "${JAVA_OPTS}" in
+  *"XX:ParallelGCThreads"*)
+  FOUNDGC=1
+  ;;
+esac
+
+if [ $FOUNDGC -eq 0 ]; then
+    JAVA_OPTS="${JAVA_OPTS} -XX:ParallelGCThreads=2"
+fi
+
+
 JAVABIN=`which java`
 if [ "${JAVA_HOME}" != "" ]; then
     JAVABIN="$JAVA_HOME/bin/java"
