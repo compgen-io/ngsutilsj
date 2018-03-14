a3d44b0e2c8f96354e9de947f31a407a274aea92
Marcus R. Breese <marcus.breese@ucsf.edu>
2018-03-14 19:02:15 -0400

    May need to populate through multiple chunks...

--- src/java/io/compgen/ngsutils/tabix/TabixQueryIterator.java
+++ src/java/io/compgen/ngsutils/tabix/TabixQueryIterator.java
@@ -34,62 +34,65 @@ public class TabixQueryIterator implements Iterator<String> {
     }
 
     private void populate() throws IOException, DataFormatException {
-        if (chunks.size() == 0) {
-            return;
-        }
-        
-        Chunk chunk = chunks.remove(0);
-        
-        byte[] chunkBuf = bgzf.readBlocks(chunk.coffsetBegin, chunk.uoffsetBegin, chunk.coffsetEnd, chunk.uoffsetEnd);
-        String s = new String(chunkBuf, "UTF-8");
-        for (String line: s.split("\n")) {
-            if (line.startsWith(""+index.getMeta())) {
-                continue;
+        while (buffer.size() == 0) {
+            if (chunks.size() == 0) {
+                return;
             }
-            String[] cols = line.split("\t");
-//              System.err.println(StringUtils.join(",", cols));
-            if (cols[index.getColSeq()-1].equals(ref)) {
-                int b = -1, e = -1;
-                try {
-                    if (index.getColEnd() > 0) {
-                        b = Integer.parseInt(cols[index.getColBegin()-1]);
-                        e = Integer.parseInt(cols[index.getColEnd()-1]);
-                    } else {
-                        b = Integer.parseInt(cols[index.getColBegin()-1]);
-                        e = Integer.parseInt(cols[index.getColBegin()-1]);;
-                    }
-                    
-                    if ((index.getFormat()&0x10000) == 0) {
-                        // convert one-based begin coord (in bgzip file)
-                        b--;
-                    }
-
-                    if (b > end) {
-                        // we are past the pos we need, so no more valid lines
-                        break;
-                    }
-                    
-                    // return if the spans overlap at all -- if necessary, the 
-                    // calling function can re-parse.
+            
+            Chunk chunk = chunks.remove(0);
+            
+            byte[] chunkBuf = bgzf.readBlocks(chunk.coffsetBegin, chunk.uoffsetBegin, chunk.coffsetEnd, chunk.uoffsetEnd);
+            String s = new String(chunkBuf, "UTF-8");
+            for (String line: s.split("\n")) {
+                if (line.startsWith(""+index.getMeta())) {
+                    continue;
+                }
+                String[] cols = line.split("\t");
+    //              System.err.println(StringUtils.join(",", cols));
+                if (cols[index.getColSeq()-1].equals(ref)) {
+                    int b = -1, e = -1;
+                    try {
+                        if (index.getColEnd() > 0) {
+                            b = Integer.parseInt(cols[index.getColBegin()-1]);
+                            e = Integer.parseInt(cols[index.getColEnd()-1]);
+                        } else {
+                            b = Integer.parseInt(cols[index.getColBegin()-1]);
+                            e = Integer.parseInt(cols[index.getColBegin()-1]);;
+                        }
+                        
+                        if ((index.getFormat()&0x10000) == 0) {
+                            // convert one-based begin coord (in bgzip file)
+                            b--;
+                        }
     
-                    if (
-                            (b <= start && start < e) || // query start is within tabix range
-                            (start <= b && e < end) ||   // tabix range is contained completely by query
-                            (b < end && end <= e)        // query end is within tabix range
-                        ) {
-                        buffer.addLast(line);     
+                        if (b > end) {
+                            // we are past the pos we need, so no more valid lines
+                            // TODO: make this shut down the iterator too??
+                            break;
+                        }
+                        
+                        // return if the spans overlap at all -- if necessary, the 
+                        // calling function can re-parse.
+        
+                        if (
+                                (b <= start && start < e) || // query start is within tabix range
+                                (start <= b && e < end) ||   // tabix range is contained completely by query
+                                (b < end && end <= e)        // query end is within tabix range
+                            ) {
+                            buffer.addLast(line);     
+                        }
+                    } catch (Exception ex) {
+                        System.err.println("ERROR chunk"+chunk.coffsetBegin +","+ chunk.uoffsetBegin +" -> "+ chunk.coffsetEnd+","+ chunk.uoffsetEnd);
+                        System.err.println("ref="+ref+", b="+b+", e="+e+", start="+start+", end="+end);
+                        System.err.println("chunkBuf.length => " + chunkBuf.length);
+                        System.err.println("line => " + line);
+                        System.err.println("cols => " + StringUtils.join(", ", cols));
+    //                        for (String l: s.split("\n")) {
+    //                            System.err.println("s => " + l);
+    //                        }
+                        ex.printStackTrace(System.err);
+                        System.exit(1);
                     }
-                } catch (Exception ex) {
-                    System.err.println("ERROR chunk"+chunk.coffsetBegin +","+ chunk.uoffsetBegin +" -> "+ chunk.coffsetEnd+","+ chunk.uoffsetEnd);
-                    System.err.println("ref="+ref+", b="+b+", e="+e+", start="+start+", end="+end);
-                    System.err.println("chunkBuf.length => " + chunkBuf.length);
-                    System.err.println("line => " + line);
-                    System.err.println("cols => " + StringUtils.join(", ", cols));
-//                        for (String l: s.split("\n")) {
-//                            System.err.println("s => " + l);
-//                        }
-                    ex.printStackTrace(System.err);
-                    System.exit(1);
                 }
             }
         }
