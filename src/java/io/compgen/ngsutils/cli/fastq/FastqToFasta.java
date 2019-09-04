1e7de6a43c9a81f00b7b217a1746ed2be25089e0
Marcus Breese <marcus@breese.com>
2019-09-04 12:17:41 -0400

    Convert more than one fastq to fasta at a time
    w/o concatenating the input files.

--- src/java/io/compgen/ngsutils/cli/fastq/FastqToFasta.java
+++ src/java/io/compgen/ngsutils/cli/fastq/FastqToFasta.java
@@ -15,35 +15,37 @@ import io.compgen.ngsutils.fastq.FastqReader;
 
 @Command(name = "fastq-tofasta", desc = "Convert FASTQ sequences to FASTA format", category="fastq")
 public class FastqToFasta extends AbstractCommand {
-	private String filename;
+	private String[] filenames;
 
 	public FastqToFasta() {
 	}
 
 	@UnnamedArg(name = "FILE")
-	public void setFilename(String filename) throws IOException {
-	    this.filename = filename;
+	public void setFilename(String[] filenames) throws IOException {
+	    this.filenames = filenames;
 	}
 	
 	@Exec
     public void exec() throws IOException {
 	    long count=0;
 
-        File file1 = new File(filename);
-        FileInputStream fis1 = new FileInputStream(file1);
-        FileChannel channel1 = fis1.getChannel();
-    
-        FastqReader reader = Fastq.open(fis1, null, channel1, filename);
-        for (FastqRead read : reader) {
-            count++;
-            if (read.getComment()!=null) {
-                System.out.println(">"+read.getName()+" " +read.getComment());
-            } else {
-                System.out.println(">"+read.getName());
+	    for (String filename: filenames) {
+            File file1 = new File(filename);
+            FileInputStream fis1 = new FileInputStream(file1);
+            FileChannel channel1 = fis1.getChannel();
+        
+            FastqReader reader = Fastq.open(fis1, null, channel1, filename);
+            for (FastqRead read : reader) {
+                count++;
+                if (read.getComment()!=null) {
+                    System.out.println(">"+read.getName()+" " +read.getComment());
+                } else {
+                    System.out.println(">"+read.getName());
+                }
+                System.out.println(read.getSeq());
             }
-            System.out.println(read.getSeq());
-        }
-
-	    System.err.println(count+" reads found.");
+    
+    	    System.err.println(count+" reads found.");
+	    }
 	}
 }
