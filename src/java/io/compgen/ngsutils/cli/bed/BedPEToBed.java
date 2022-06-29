680088e3cb74f1d41f0ff27cb11fe086dc74f54f
Marcus R. Breese <marcus@breese.com>
2022-06-29 11:21:41 -0400

    added --first --second and --max=-1 to disable check

--- src/java/io/compgen/ngsutils/cli/bed/BedPEToBed.java
+++ src/java/io/compgen/ngsutils/cli/bed/BedPEToBed.java
@@ -21,13 +21,25 @@ public class BedPEToBed extends AbstractOutputCommand {
     private String filename = null;
     private int maxDistance = 0;
     private boolean split = false;
+    private boolean first = false;
+    private boolean second = false;
     
     @Option(desc = "Split records into two BED lines", name="split")
     public void setSplit(boolean split) {
     	this.split = split;
     }
 
-    @Option(desc = "Maximum allowed distance between records", name="max", defaultValue="10000")
+    @Option(desc = "Write the first record only", name="first")
+    public void setFirst(boolean first) {
+    	this.first = first;
+    }
+
+    @Option(desc = "Write the second record only", name="second")
+    public void setSecond(boolean second) {
+    	this.second = second;
+    }
+
+    @Option(desc = "Maximum allowed distance between records (-1 to disable)", name="max", defaultValue="10000")
     public void setMaxDistance(int maxDistance) {
         this.maxDistance = maxDistance;
     }
@@ -39,6 +51,21 @@ public class BedPEToBed extends AbstractOutputCommand {
 
     @Exec
     public void exec() throws IOException, CommandArgumentException {
+    	int i = 0;
+    	if (first) {
+    		i++;
+    	}
+    	if (second) {
+    		i++;
+    	}
+    	if (split) {
+    		i++;
+    	}
+    	
+    	if (i > 1) {
+    		throw new CommandArgumentException("You can only specify one of: --first --second --split");
+    	}
+    	
         for (BedPERecord record: IterUtils.wrap(BedPEReader.readFile(filename))) {
             
         	GenomeSpan coord1 = record.getCoord1();
@@ -47,6 +74,10 @@ public class BedPEToBed extends AbstractOutputCommand {
             if (split) {
             	new BedRecord(new GenomeSpan(coord1.ref, coord1.start, coord1.end), record.getName(), record.getScoreAsDouble(), record.getExtras()).write(out);
             	new BedRecord(new GenomeSpan(coord2.ref, coord2.start, coord2.end), record.getName(), record.getScoreAsDouble(), record.getExtras()).write(out);
+            } else if (first) {
+                new BedRecord(new GenomeSpan(coord1.ref, coord1.start, coord1.end), record.getName(), record.getScoreAsDouble(), record.getExtras()).write(out);
+            } else if (second) {
+                new BedRecord(new GenomeSpan(coord2.ref, coord2.start, coord2.end), record.getName(), record.getScoreAsDouble(), record.getExtras()).write(out);
             } else {
 	            if (isDiscordant(coord1, coord2, maxDistance)) {
 	            	continue;            	
@@ -63,12 +94,12 @@ public class BedPEToBed extends AbstractOutputCommand {
     public static boolean isDiscordant(GenomeSpan coord1, GenomeSpan coord2, int maxDist) {
     	if (coord1.ref.equals(coord2.ref)) {
     		if (coord1.compareTo(coord2) <  0) { // coord1 is less
-    			if (coord2.start - coord1.end <= maxDist) {
+    			if (maxDist == -1 || coord2.start - coord1.end <= maxDist) {
     				// concordant reads are on different strands
     				return coord1.strand != Strand.NONE && coord2.strand != Strand.NONE && coord2.strand.equals(coord1.strand); 
     			}
     		} else {
-    			if (coord1.start - coord2.end <= maxDist) { // coord2 is less or equal
+    			if (maxDist == -1 || coord1.start - coord2.end <= maxDist) { // coord2 is less or equal
     				// concordant reads are on different strands
     				return coord1.strand != Strand.NONE && coord2.strand != Strand.NONE && coord2.strand.equals(coord1.strand); 
     			}
