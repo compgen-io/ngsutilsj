0f0b3f327a5dac53aa67b90a464ed195e25b8f03
Marcus Breese <marcus@breese.com>
2022-11-06 00:07:43 -0400

    split out FAIFile into a separate class

--- src/java/io/compgen/ngsutils/fasta/IndexedFastaFile.java
+++ src/java/io/compgen/ngsutils/fasta/IndexedFastaFile.java
@@ -3,58 +3,29 @@ package io.compgen.ngsutils.fasta;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-
-import io.compgen.common.StringLineReader;
-import io.compgen.common.StringUtils;
+import java.util.List;
 
 public class IndexedFastaFile extends BasicFastaReader {
-    public class IndexRecord {
-        public final String name;
-        public final long length;
-        public final long offset;
-        public final int lineSeqLength;
-        public final int lineOffsetLength;
+    private FAIFile fai = null;
+	public RandomAccessFile file;
 
-        public IndexRecord(String name, long length, long offset, int lineSeqLength,
-                int lineOffsetLength) {
-            this.name = name;
-            this.length = length;
-            this.offset = offset;
-            this.lineSeqLength = lineSeqLength;
-            this.lineOffsetLength = lineOffsetLength;
-        }
-    }
-    
-    private RandomAccessFile file;
-
-    private Map<String, IndexRecord> indexMap = new LinkedHashMap<String, IndexRecord>();
-    
-    public IndexedFastaFile(String filename) throws IOException {
+	public IndexedFastaFile(String filename) throws IOException {
         super(filename);
         
         if (!new File(filename+".fai").exists()) {
             throw new IOException("Missing FAI index file!");
         }
         
-        file = new RandomAccessFile(filename, "r");
-        for (String line: new StringLineReader(filename+".fai")) {
-            String[] cols = StringUtils.strip(line).split("\t");
-            indexMap.put(cols[0], new IndexRecord(cols[0], Long.parseLong(cols[1]), Long.parseLong(cols[2]), Integer.parseInt(cols[3]), Integer.parseInt(cols[4])));
-        }
+        this.fai = new FAIFile(filename + ".fai");        
+        this.file = new RandomAccessFile(filename, "r");
     }
     
-    public Set<String> getReferenceNames() {
-        return indexMap.keySet();
+    public List<String> getReferenceNames() {
+        return fai.getNames();
     }
     
     public long getReferenceLength(String name) {
-        if (indexMap.containsKey(name)) {
-            return indexMap.get(name).length;
-        }
-        return -1; 
+    	return fai.getLength(name);
     }
     
     /*
@@ -65,13 +36,18 @@ public class IndexedFastaFile extends BasicFastaReader {
      */
     @Override
     public String fetchSequence(String ref, int start, int end) throws IOException {
-        if (!indexMap.containsKey(ref)) {
+        if (!fai.contains(ref)) {
             throw new RuntimeException("Invalid reference name! \""+ref+"\" not found in FASTA file!");
         }
-        long pos = indexMap.get(ref).offset;
-        int linenum = start / indexMap.get(ref).lineSeqLength;
-        int lineoff = start % indexMap.get(ref).lineSeqLength;
-        pos += linenum * indexMap.get(ref).lineOffsetLength + lineoff;
+        
+        long offset = fai.getOffset(ref);
+        int lineSeqLength = fai.getLineSeqLength(ref);
+        int lineOffsetLength = fai.getLineOffsetLength(ref);
+        
+        long pos = offset;
+        int linenum = start / lineSeqLength;
+        int lineoff = start % lineSeqLength;
+        pos += linenum * lineOffsetLength + lineoff;
         
         String out = "";
         
@@ -82,8 +58,8 @@ public class IndexedFastaFile extends BasicFastaReader {
             chars++;
             lineoff++;
             
-            if (lineoff >= indexMap.get(ref).lineSeqLength) {
-                file.skipBytes(indexMap.get(ref).lineOffsetLength - indexMap.get(ref).lineSeqLength);
+            if (lineoff >= lineSeqLength) {
+                file.skipBytes(lineOffsetLength - lineSeqLength);
                 lineoff = 0;
             }
         }
